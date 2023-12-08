ae87716f614f9af5074950dcd503e20ff003de59
LENOVO <LENOVO@DESKTOP-CEPDD2L>
2023-12-09 00:10:42 +0530

    second commit

---
+++ raviraajdemo/src/sampledemo/TestCase2.java
@@ -0,0 +1,11 @@
+package sampledemo;
+
+public class TestCase2 {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("This is the second test case for regreestion testing");
+
+	}
+
+}
